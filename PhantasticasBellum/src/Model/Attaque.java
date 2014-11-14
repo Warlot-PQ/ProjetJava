5c0d9025d1d24c6316aa842c89e684033f5ca478
Gasquez Thomas <tgasquez@ONEIL-a-11.enssat.fr>
2014-11-14 10:15:20 +0100

    Attaque MaJ

--- PhantasticasBellum/src/Model/Attaque.java
+++ PhantasticasBellum/src/Model/Attaque.java
@@ -3,21 +3,39 @@ package Model;
 import java.util.List;
 
 public class Attaque {
-	private String Nom;
+	private String nom;
 	private int degat;
 	private int portee;
 	private Matrice zone;
+	private Effet effetProduit = null;
 	
-	public Attaque(){
-		
+	public Attaque(String nom, int degat, int portee, Matrice zone, Effet effetProduit){
+		this.nom = nom;
+		this.degat = degat;
+		this.portee = portee;
+		this.zone = zone;
+		this.effetProduit = effetProduit;
+	}
+	
+	public Effet getEffetProduit() {
+		return effetProduit;
 	}
 
+	public void setEffetProduit(Effet effetProduit) {
+		this.effetProduit = effetProduit;
+	}
+
+	public Attaque(String nom, int degat, int portee, Matrice zone){
+		this(nom, degat, portee, zone,  null);
+	}
+
+
 	public String getNom() {
-		return Nom;
+		return nom;
 	}
 
 	public void setNom(String nom) {
-		Nom = nom;
+		this.nom = nom;
 	}
 
 	public int getDegat() {
